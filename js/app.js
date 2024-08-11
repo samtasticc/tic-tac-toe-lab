2b6b33e1a787ebda51e06b7ce8a4a1e91aa7146f
samtasticc <samantha.henesy@gmail.com>
2024-08-11 14:21:23 -0400

    set up css, js, and index pages

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*-------------------------------- Constants --------------------------------*/
+// values that cannot be reassigned once they are declared (const)
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// containers for storing data (const, let)
+
+
+/*------------------------ Cached Element References ------------------------*/
+// when you access an element in the DOM
+
+
+/*-------------------------------- Functions --------------------------------*/
+// reusuable blocks of code designed to perform a specific task
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+// a function that 'listens' for a specific event to occur
+
+
